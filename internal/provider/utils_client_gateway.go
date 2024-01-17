4c76d4547ea1d9d15e857afc9cbdd1e7e55c0155
Thomas Saison <thomas.saison-ext@numspot.com>
2024-01-17 16:10:57 +0100

    client_gateway

--- internal/provider/utils_client_gateway.go
+++ internal/provider/utils_client_gateway.go
@@ -1,18 +1,36 @@
 package provider
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/conns/api"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/resource_client_gateway"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
 func ClientGatewayFromTfToHttp(tf resource_client_gateway.ClientGatewayModel) *api.ClientGatewaySchema {
-	return &api.ClientGatewaySchema{}
+	return &api.ClientGatewaySchema{
+		BgpAsn:         utils.FromTfInt64ToIntPtr(tf.BgpAsn),
+		ConnectionType: tf.ConnectionType.ValueStringPointer(),
+		Id:             tf.Id.ValueStringPointer(),
+		PublicIp:       tf.PublicIp.ValueStringPointer(),
+		State:          tf.State.ValueStringPointer(),
+	}
 }
 
 func ClientGatewayFromHttpToTf(http *api.ClientGatewaySchema) resource_client_gateway.ClientGatewayModel {
-	return resource_client_gateway.ClientGatewayModel{}
+	return resource_client_gateway.ClientGatewayModel{
+		BgpAsn:         utils.FromIntPtrToTfInt64(http.BgpAsn),
+		ConnectionType: types.StringPointerValue(http.ConnectionType),
+		Id:             types.StringPointerValue(http.Id),
+		PublicIp:       types.StringPointerValue(http.PublicIp),
+		State:          types.StringPointerValue(http.State),
+	}
 }
 
 func ClientGatewayFromTfToCreateRequest(tf resource_client_gateway.ClientGatewayModel) api.CreateClientGatewayJSONRequestBody {
-	return api.CreateClientGatewayJSONRequestBody{}
+	return api.CreateClientGatewayJSONRequestBody{
+		BgpAsn:         utils.FromTfInt64ToIntPtr(tf.BgpAsn),
+		ConnectionType: tf.ConnectionType.ValueStringPointer(),
+		PublicIp:       tf.PublicIp.ValueStringPointer(),
+	}
 }
