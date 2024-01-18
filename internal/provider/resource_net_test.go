1f8d2863c6039e7b1e3080cb85847ebd284bd170
Safouane Fakhfakh <safouane.fakhfakh@numspot.com>
2024-01-18 11:37:15 +0100

    vpc test

--- internal/provider/resource_net_test.go
+++ internal/provider/resource_net_test.go
@@ -1,23 +1,25 @@
 package provider
 
 import (
-  "fmt"
-  "testing"
+	"fmt"
+	"testing"
 
-  "github.com/hashicorp/terraform-plugin-testing/helper/resource"
-  "github.com/stretchr/testify/require"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccNetResource(t *testing.T) {
+	ipRange := "10.0.0.0/16"
+	ipRangeUpdated := "10.0.0.1/16"
 	pr := TestAccProtoV6ProviderFactories
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: pr,
 		Steps: []resource.TestStep{
 			{
-				Config: testNetConfig_Create(),
+				Config: testNetConfig_Create(ipRange),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("numspot_net.test", "field", "value"),
-					resource.TestCheckResourceAttrWith("numspot_net.test", "field", func(v string) error {
+					resource.TestCheckResourceAttr("numspot_net.test", "ip_range", ipRange),
+					resource.TestCheckResourceAttrWith("numspot_net.test", "id", func(v string) error {
 						require.NotEmpty(t, v)
 						return nil
 					}),
@@ -25,17 +27,17 @@ func TestAccNetResource(t *testing.T) {
 			},
 			// ImportState testing
 			{
-				ResourceName:      "numspot_net.test",
-				ImportState:       true,
-				ImportStateVerify: true,
+				ResourceName:            "numspot_net.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{},
 			},
 			// Update testing
 			{
-				Config: testNetConfig_Update(),
+				Config: testNetConfig_Update(ipRangeUpdated),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("numspot_net.test", "field", "value"),
-					resource.TestCheckResourceAttrWith("numspot_net.test", "field", func(v string) error {
+					resource.TestCheckResourceAttr("numspot_net.test", "ip_range", ipRangeUpdated),
+					resource.TestCheckResourceAttrWith("numspot_net.test", "id", func(v string) error {
 						return nil
 					}),
 				),
@@ -43,11 +45,13 @@ func TestAccNetResource(t *testing.T) {
 		},
 	})
 }
-func testNetConfig_Create() string {
+func testNetConfig_Create(ipRange string) string {
 	return fmt.Sprintf(`resource "numspot_net" "test" {
-  			}`)
+			ip_range=%s
+  			}`, ipRange)
 }
-func testNetConfig_Update() string {
-		return `resource "numspot_net" "test" {
-    			}`
+func testNetConfig_Update(ipRange string) string {
+	return fmt.Sprintf(`resource "numspot_net" "test" {
+			ip_range=%s
+  			}`, ipRange)
 }
