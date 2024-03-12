cf5986d10352c3b72b49b5a660501ac212cd53c2
Safouane Fakhfakh <safouane.fakhfakh@numspot.com>
2024-03-12 14:15:04 +0100

    chore: add tags to vpc datasource

--- internal/provider/datasource_vpcs_test.go
+++ internal/provider/datasource_vpcs_test.go
@@ -1,34 +1,77 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAccVPCsDatasource(t *testing.T) {
+func TestAccVPCsDatasource_Basic(t *testing.T) {
 	pr := TestAccProtoV6ProviderFactories
+	ipRange := "10.101.0.0/16"
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: pr,
 		Steps: []resource.TestStep{
 			{
-				Config: fetchVPCsConfig(),
+				Config: testAccVPCsDatasourceConfig_Basic(ipRange),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.numspot_vpcs.testdata", "vpcs.#", "1"),
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.#", "1"),
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.0.ip_range", ipRange),
 				),
 			},
 		},
 	})
 }
 
-func fetchVPCsConfig() string {
-	return `
-resource "numspot_vpc" "test" {
-	ip_range = "10.101.0.0/16"
+func TestAccVPCsDatasource_WithTags(t *testing.T) {
+	pr := TestAccProtoV6ProviderFactories
+	ipRange := "10.101.0.0/16"
+	tagName := "Name"
+	tagValue := "dhcp_numspot"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: pr,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCsDatasourceConfig_WithTags(ipRange, tagName, tagValue),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.#", "1"),
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.0.ip_range", ipRange),
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.0.tags.0.key", tagName),
+					resource.TestCheckResourceAttr("data.numspot_vpcs.test", "vpcs.0.tags.0.value", tagValue),
+				),
+			},
+		},
+	})
 }
 
-data "numspot_vpcs" "testdata" {
+func testAccVPCsDatasourceConfig_Basic(ipRange string) string {
+	return fmt.Sprintf(`
+resource "numspot_vpc" "test" {
+	ip_range = %[1]q
+}
+
+data "numspot_vpcs" "test" {
 	ids = [numspot_vpc.test.id]
-}`
+}`, ipRange)
+}
+func testAccVPCsDatasourceConfig_WithTags(ipRange, tagName, tagValue string) string {
+	return fmt.Sprintf(`
+resource "numspot_vpc" "test" {
+	ip_range = %[1]q
+	tags = [
+			{
+				key = %[2]q
+				value = %[3]q
+			}
+		]
+}
+
+data "numspot_vpcs" "test" {
+	tags = [
+format("%%s=%%s", numspot_vpc.test.tags[0].key, numspot_vpc.test.tags[0].value)
+]
+}`, ipRange, tagName, tagValue)
 }
