123e9efddd91e9f558ee05f5615895e76cdb0901
Victor Duvert <victor.duvert-ext@numspot.com>
2024-04-23 14:48:53 +0000

    Vpc peering datasource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -386,6 +386,7 @@ func (p *numspotProvider) DataSources(ctx context.Context) []func() datasource.D
 		NewVirtualGatewaysDataSource,
 		NewNicsDataSource,
 		NewNatGatewaysDataSource,
+		NewVpcPeeringsDataSource,
 		NewInternetGatewaysDataSource,
 		NewSnapshotDataSource,
 		NewKeypairDataSource,
--- internal/provider/utils_vpc_peering.go
+++ internal/provider/utils_vpc_peering.go
@@ -2,13 +2,17 @@ package provider
 
 import (
 	"context"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"gitlab.numspot.cloud/cloud/numspot-sdk-go/pkg/iaas"
 
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/datasource_vpc_peering"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/resource_vpc_peering"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/tags"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
 func accepterVpcFromApi(ctx context.Context, http *iaas.AccepterVpc) (resource_vpc_peering.AccepterVpcValue, diag.Diagnostics) {
@@ -116,4 +120,91 @@ func VpcPeeringFromTfToCreateRequest(tf resource_vpc_peering.VpcPeeringModel) ia
 		AccepterVpcId: tf.AccepterVpcId.ValueString(),
 		SourceVpcId:   tf.SourceVpcId.ValueString(),
 	}
+}
+
+func VpcPeeringsFromTfToAPIReadParams(ctx context.Context, tf VpcPeeringsDataSourceModel) iaas.ReadVpcPeeringsParams {
+	expirationDates := utils.TfStringListToTimeList(ctx, tf.ExpirationDates, "2020-06-30T00:00:00.000Z")
+
+	return iaas.ReadVpcPeeringsParams{
+		ExpirationDates:       &expirationDates,
+		StateMessages:         utils.TfStringListToStringPtrList(ctx, tf.StateMessages),
+		StateNames:            utils.TfStringListToStringPtrList(ctx, tf.StateNames),
+		AccepterVpcAccountIds: utils.TfStringListToStringPtrList(ctx, tf.AccepterVpcAccountIds),
+		AccepterVpcIpRanges:   utils.TfStringListToStringPtrList(ctx, tf.AccepterVpcIpRanges),
+		AccepterVpcVpcIds:     utils.TfStringListToStringPtrList(ctx, tf.AccepterVpcVpcIds),
+		Ids:                   utils.TfStringListToStringPtrList(ctx, tf.IDs),
+		SourceVpcAccountIds:   utils.TfStringListToStringPtrList(ctx, tf.SourceVpcAccountIds),
+		SourceVpcIpRanges:     utils.TfStringListToStringPtrList(ctx, tf.SourceVpcIpRanges),
+		SourceVpcVpcIds:       utils.TfStringListToStringPtrList(ctx, tf.SourceVpcVpcIds),
+		TagKeys:               utils.TfStringListToStringPtrList(ctx, tf.TagKeys),
+		TagValues:             utils.TfStringListToStringPtrList(ctx, tf.TagValues),
+		Tags:                  utils.TfStringListToStringPtrList(ctx, tf.Tags),
+	}
+}
+
+func VpcPeeringsFromHttpToTfDatasource(ctx context.Context, http *iaas.VpcPeering) (*datasource_vpc_peering.VpcPeeringModel, diag.Diagnostics) {
+	var (
+		diags            diag.Diagnostics
+		tagsList         types.List
+		accepterVpc      datasource_vpc_peering.AccepterVpcValue
+		sourceVpc        datasource_vpc_peering.SourceVpcValue
+		state            datasource_vpc_peering.StateValue
+		expirationDateTf types.String
+	)
+
+	if http.Tags != nil {
+		tagsList, diags = utils.GenericListToTfListValue(ctx, tags.TagsValue{}, tags.ResourceTagFromAPI, *http.Tags)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.ExpirationDate != nil {
+		expirationDate := *http.ExpirationDate
+		expirationDateTf = types.StringValue(expirationDate.Format(time.RFC3339))
+	}
+
+	if http.AccepterVpc != nil {
+		accepterVpc, diags = datasource_vpc_peering.NewAccepterVpcValue(datasource_vpc_peering.AccepterVpcValue{}.AttributeTypes(ctx),
+			map[string]attr.Value{
+				"account_id": types.StringPointerValue(http.AccepterVpc.AccountId),
+				"ip_range":   types.StringPointerValue(http.AccepterVpc.IpRange),
+				"vpc_id":     types.StringPointerValue(http.AccepterVpc.VpcId),
+			})
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.SourceVpc != nil {
+		sourceVpc, diags = datasource_vpc_peering.NewSourceVpcValue(datasource_vpc_peering.SourceVpcValue{}.AttributeTypes(ctx),
+			map[string]attr.Value{
+				"account_id": types.StringPointerValue(http.SourceVpc.AccountId),
+				"ip_range":   types.StringPointerValue(http.SourceVpc.IpRange),
+				"vpc_id":     types.StringPointerValue(http.SourceVpc.VpcId),
+			})
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.State != nil {
+		state, diags = datasource_vpc_peering.NewStateValue(datasource_vpc_peering.StateValue{}.AttributeTypes(ctx),
+			map[string]attr.Value{
+				"message": types.StringPointerValue(http.State.Message),
+				"name":    types.StringPointerValue(http.State.Name),
+			})
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	return &datasource_vpc_peering.VpcPeeringModel{
+		Id:             types.StringPointerValue(http.Id),
+		Tags:           tagsList,
+		AccepterVpc:    accepterVpc,
+		ExpirationDate: expirationDateTf,
+		SourceVpc:      sourceVpc,
+		State:          state,
+	}, nil
 }
