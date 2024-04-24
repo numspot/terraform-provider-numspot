85ee29748dbda1f1eaa501bc93726e63c00b6281
Victor Duvert <victor.duvert-ext@numspot.com>
2024-04-24 13:31:33 +0000

    Route Table datasource

---
+++ internal/provider/datasource_route_table.go
@@ -0,0 +1,122 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"gitlab.numspot.cloud/cloud/numspot-sdk-go/pkg/iaas"
+
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/datasource_route_table"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
+)
+
+type RouteTablesDataSourceModel struct {
+	RouteTables                 []datasource_route_table.RouteTableModel `tfsdk:"route_tables"`
+	TagKeys                     types.List                               `tfsdk:"tag_keys"`
+	TagValues                   types.List                               `tfsdk:"tag_values"`
+	Tags                        types.List                               `tfsdk:"tags"`
+	Ids                         types.List                               `tfsdk:"ids"`
+	RouteVpcPeeringIds          types.List                               `tfsdk:"route_vpc_peering_ids"`
+	RouteNatGatewayIds          types.List                               `tfsdk:"route_nat_gateway_ids"`
+	RouteVmIds                  types.List                               `tfsdk:"route_vm_ids"`
+	RouteCreationMethods        types.List                               `tfsdk:"route_creation_methods"`
+	RouteDestinationIpRanges    types.List                               `tfsdk:"route_destination_ip_ranges"`
+	RouteDestinationServiceIds  types.List                               `tfsdk:"route_destination_service_ids"`
+	RouteGatewayIds             types.List                               `tfsdk:"route_gateway_ids"`
+	RouteStates                 types.List                               `tfsdk:"route_states"`
+	VpcIds                      types.List                               `tfsdk:"vpc_ids"`
+	LinkRouteTableIds           types.List                               `tfsdk:"link_route_table_ids"`
+	LinkRouteTableMain          types.Bool                               `tfsdk:"link_route_table_main"`
+	LinkRouteTableRouteTableIds types.List                               `tfsdk:"link_route_table_route_table_ids"`
+	LinkRouteTableSubnetIds     types.List                               `tfsdk:"link_route_table_subnet_ids"`
+}
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource = &routeTablesDataSource{}
+)
+
+func (d *routeTablesDataSource) Configure(_ context.Context, request datasource.ConfigureRequest, response *datasource.ConfigureResponse) {
+	if request.ProviderData == nil {
+		return
+	}
+
+	provider, ok := request.ProviderData.(Provider)
+	if !ok {
+		response.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", request.ProviderData),
+		)
+
+		return
+	}
+
+	d.provider = provider
+}
+
+func NewRouteTablesDataSource() datasource.DataSource {
+	return &routeTablesDataSource{}
+}
+
+type routeTablesDataSource struct {
+	provider Provider
+}
+
+// Metadata returns the data source type name.
+func (d *routeTablesDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_route_table"
+}
+
+// Schema defines the schema for the data source.
+func (d *routeTablesDataSource) Schema(ctx context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = datasource_route_table.RouteTableDataSourceSchema(ctx)
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (d *routeTablesDataSource) Read(ctx context.Context, request datasource.ReadRequest, response *datasource.ReadResponse) {
+	var state, plan RouteTablesDataSourceModel
+	request.Config.Get(ctx, &plan)
+
+	params := RouteTablesFromTfToAPIReadParams(ctx, plan)
+	res := utils.ExecuteRequest(func() (*iaas.ReadRouteTablesResponse, error) {
+		return d.provider.ApiClient.ReadRouteTablesWithResponse(ctx, d.provider.SpaceID, &params)
+	}, http.StatusOK, &response.Diagnostics)
+	if res == nil {
+		return
+	}
+	if res.JSON200.Items == nil {
+		response.Diagnostics.AddError("HTTP call failed", "got empty Route Table list")
+	}
+
+	for _, item := range *res.JSON200.Items {
+		tf, diags := RouteTablesFromHttpToTfDatasource(ctx, &item)
+		if diags != nil {
+			response.Diagnostics.AddError("Error while converting Route Table HTTP object to Terraform object", diags.Errors()[0].Detail())
+			return
+		}
+		state.RouteTables = append(state.RouteTables, *tf)
+	}
+
+	state.TagKeys = plan.TagKeys
+	state.TagValues = plan.TagValues
+	state.Tags = plan.Tags
+	state.Ids = plan.Ids
+	state.RouteVpcPeeringIds = plan.RouteVpcPeeringIds
+	state.RouteNatGatewayIds = plan.RouteNatGatewayIds
+	state.RouteVmIds = plan.RouteVmIds
+	state.RouteCreationMethods = plan.RouteCreationMethods
+	state.RouteDestinationIpRanges = plan.RouteDestinationIpRanges
+	state.RouteDestinationServiceIds = plan.RouteDestinationServiceIds
+	state.RouteGatewayIds = plan.RouteGatewayIds
+	state.RouteStates = plan.RouteStates
+	state.VpcIds = plan.VpcIds
+	state.LinkRouteTableIds = plan.LinkRouteTableIds
+	state.LinkRouteTableMain = plan.LinkRouteTableMain
+	state.LinkRouteTableRouteTableIds = plan.LinkRouteTableRouteTableIds
+	state.LinkRouteTableSubnetIds = plan.LinkRouteTableSubnetIds
+
+	response.Diagnostics.Append(response.State.Set(ctx, state)...)
+}
--- internal/provider/utils_route_table.go
+++ internal/provider/utils_route_table.go
@@ -8,7 +8,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"gitlab.numspot.cloud/cloud/numspot-sdk-go/pkg/iaas"
 
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/datasource_route_table"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/resource_route_table"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider/tags"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
@@ -85,4 +87,117 @@ func RouteTableFromTfToCreateRequest(tf *resource_route_table.RouteTableModel) i
 	return iaas.CreateRouteTableJSONRequestBody{
 		VpcId: tf.VpcId.ValueString(),
 	}
+}
+
+func RouteTablesFromTfToAPIReadParams(ctx context.Context, tf RouteTablesDataSourceModel) iaas.ReadRouteTablesParams {
+	return iaas.ReadRouteTablesParams{
+		TagKeys:                         utils.TfStringListToStringPtrList(ctx, tf.TagKeys),
+		TagValues:                       utils.TfStringListToStringPtrList(ctx, tf.TagValues),
+		Tags:                            utils.TfStringListToStringPtrList(ctx, tf.Tags),
+		Ids:                             utils.TfStringListToStringPtrList(ctx, tf.Ids),
+		RouteVpcPeeringIds:              utils.TfStringListToStringPtrList(ctx, tf.RouteVpcPeeringIds),
+		RouteNatGatewayIds:              utils.TfStringListToStringPtrList(ctx, tf.RouteNatGatewayIds),
+		RouteVmIds:                      utils.TfStringListToStringPtrList(ctx, tf.RouteVmIds),
+		RouteCreationMethods:            utils.TfStringListToStringPtrList(ctx, tf.RouteCreationMethods),
+		RouteDestinationIpRanges:        utils.TfStringListToStringPtrList(ctx, tf.RouteDestinationIpRanges),
+		RouteDestinationServiceIds:      utils.TfStringListToStringPtrList(ctx, tf.RouteDestinationServiceIds),
+		RouteGatewayIds:                 utils.TfStringListToStringPtrList(ctx, tf.RouteGatewayIds),
+		RouteStates:                     utils.TfStringListToStringPtrList(ctx, tf.RouteStates),
+		VpcIds:                          utils.TfStringListToStringPtrList(ctx, tf.VpcIds),
+		LinkRouteTableIds:               utils.TfStringListToStringPtrList(ctx, tf.LinkRouteTableIds),
+		LinkRouteTableMain:              utils.FromTfBoolToBoolPtr(tf.LinkRouteTableMain),
+		LinkRouteTableLinkRouteTableIds: utils.TfStringListToStringPtrList(ctx, tf.LinkRouteTableRouteTableIds),
+		LinkSubnetIds:                   utils.TfStringListToStringPtrList(ctx, tf.LinkRouteTableSubnetIds),
+	}
+}
+
+func RouteTablesFromHttpToTfDatasource(ctx context.Context, http *iaas.RouteTable) (*datasource_route_table.RouteTableModel, diag.Diagnostics) {
+	var (
+		diags                               diag.Diagnostics
+		tagsList                            = types.ListNull(tags.TagsValue{}.Type(ctx))
+		linkRouteTablesList                 = types.ListNull(datasource_route_table.LinkRouteTablesValue{}.Type(ctx))
+		routePropagatingVirtualGatewaysList = types.ListNull(datasource_route_table.RoutePropagatingVirtualGatewaysValue{}.Type(ctx))
+		routesList                          = types.ListNull(datasource_route_table.RoutesValue{}.Type(ctx))
+	)
+
+	if http.Tags != nil {
+		tagsList, diags = utils.GenericListToTfListValue(ctx, tags.TagsValue{}, tags.ResourceTagFromAPI, *http.Tags)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.LinkRouteTables != nil {
+		linkRouteTablesList, diags = utils.GenericListToTfListValue(ctx, datasource_route_table.LinkRouteTablesValue{}, routeTableLinkFromAPIDatasource, *http.LinkRouteTables)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.RoutePropagatingVirtualGateways != nil {
+		routePropagatingVirtualGatewaysList, diags = utils.GenericListToTfListValue(
+			ctx, datasource_route_table.RoutePropagatingVirtualGatewaysValue{},
+			routeTableRoutePropagatingVirtualGatewaysFromAPIDatasource,
+			*http.RoutePropagatingVirtualGateways)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	if http.Routes != nil {
+		routesList, diags = utils.GenericListToTfListValue(ctx, datasource_route_table.RoutesValue{}, routeTableRouteFromAPIDatasource, *http.Routes)
+		if diags.HasError() {
+			return nil, diags
+		}
+	}
+
+	return &datasource_route_table.RouteTableModel{
+		Id:                              types.StringPointerValue(http.Id),
+		Tags:                            tagsList,
+		LinkRouteTables:                 linkRouteTablesList,
+		RoutePropagatingVirtualGateways: routePropagatingVirtualGatewaysList,
+		Routes:                          routesList,
+		VpcId:                           types.StringPointerValue(http.VpcId),
+	}, nil
+}
+
+func routeTableLinkFromAPIDatasource(ctx context.Context, link iaas.LinkRouteTable) (datasource_route_table.LinkRouteTablesValue, diag.Diagnostics) {
+	return datasource_route_table.NewLinkRouteTablesValue(
+		datasource_route_table.LinkRouteTablesValue{}.AttributeTypes(ctx),
+		map[string]attr.Value{
+			"id":             types.StringPointerValue(link.Id),
+			"main":           types.BoolPointerValue(link.Main),
+			"route_table_id": types.StringPointerValue(link.RouteTableId),
+			"subnet_id":      types.StringPointerValue(link.SubnetId),
+			"vpc_id":         types.StringPointerValue(link.VpcId),
+		},
+	)
+}
+
+func routeTableRouteFromAPIDatasource(ctx context.Context, route iaas.Route) (datasource_route_table.RoutesValue, diag.Diagnostics) {
+	return datasource_route_table.NewRoutesValue(
+		datasource_route_table.RoutesValue{}.AttributeTypes(ctx),
+		map[string]attr.Value{
+			"creation_method":        types.StringPointerValue(route.CreationMethod),
+			"destination_ip_range":   types.StringPointerValue(route.DestinationIpRange),
+			"destination_service_id": types.StringPointerValue(route.DestinationServiceId),
+			"gateway_id":             types.StringPointerValue(route.GatewayId),
+			"nat_gateway_id":         types.StringPointerValue(route.NatGatewayId),
+			"vpc_access_point_id":    types.StringPointerValue(route.VpcAccessPointId),
+			"vpc_peering_id":         types.StringPointerValue(route.VpcPeeringId),
+			"nic_id":                 types.StringPointerValue(route.NicId),
+			"state":                  types.StringPointerValue(route.State),
+			"vm_id":                  types.StringPointerValue(route.VmId),
+			"vm_account_id":          types.StringPointerValue(route.VmAccountId),
+		},
+	)
+}
+
+func routeTableRoutePropagatingVirtualGatewaysFromAPIDatasource(ctx context.Context, route iaas.RoutePropagatingVirtualGateway) (datasource_route_table.RoutePropagatingVirtualGatewaysValue, diag.Diagnostics) {
+	return datasource_route_table.NewRoutePropagatingVirtualGatewaysValue(
+		datasource_route_table.RoutePropagatingVirtualGatewaysValue{}.AttributeTypes(ctx),
+		map[string]attr.Value{
+			"virtual_gateway_id": types.StringPointerValue(route.VirtualGatewayId),
+		},
+	)
 }
