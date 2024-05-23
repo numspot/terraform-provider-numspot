3c2764caf03aa974fff282aad114d55387361dfa
Thomas Saison <thomas.saison-ext@numspot.com>
2024-05-23 10:44:08 +0200

    chore: refactor flexible gpu tests

--- internal/provider/resource_flexible_gpu_test.go
+++ internal/provider/resource_flexible_gpu_test.go
@@ -3,26 +3,29 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAccFlexibleGpuResource(t *testing.T) {
 	t.Parallel()
 	pr := TestAccProtoV6ProviderFactories
+
+	flexibleGpuModelName := "nvidia-a100-80"
+	flexibleGpuGeneration := "v6"
+	flexibleGpuAZ := "cloudgouv-eu-west-1a"
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: pr,
 		Steps: []resource.TestStep{
 			{
-				Config: testFlexibleGpuConfig(),
+				Config: testFlexibleGpuConfig(flexibleGpuModelName, flexibleGpuGeneration, flexibleGpuAZ),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("numspot_flexible_gpu.test", "field", "value"),
-					resource.TestCheckResourceAttrWith("numspot_flexible_gpu.test", "field", func(v string) error {
-						require.NotEmpty(t, v)
-						return nil
-					}),
+					resource.TestCheckResourceAttr("numspot_flexible_gpu.test", "model_name", flexibleGpuModelName),
+					resource.TestCheckResourceAttr("numspot_flexible_gpu.test", "generation", flexibleGpuGeneration),
+					resource.TestCheckResourceAttr("numspot_flexible_gpu.test", "availability_zone_name", flexibleGpuAZ),
 				),
 			},
 			// ImportState testing
@@ -46,11 +49,11 @@ func TestAccFlexibleGpuResource(t *testing.T) {
 	})
 }
 
-func testFlexibleGpuConfig() string {
-	return `
+func testFlexibleGpuConfig(modelName, generation, az string) string {
+	return fmt.Sprintf(`
 resource "numspot_flexible_gpu" "test" {
-  model_name             = "nvidia-a100-80"
-  generation             = "v6"
-  availability_zone_name = "eu-west-2a"
-}`
+  model_name             = %[1]q
+  generation             = %[2]q
+  availability_zone_name = %[3]q
+}`, modelName, generation, az)
 }
