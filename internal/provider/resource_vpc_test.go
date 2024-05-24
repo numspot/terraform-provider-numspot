3d9317c0cbd557e2049fe8bd89a7fa97521da7ac
Thomas Saison <thomas.saison-ext@numspot.com>
2024-05-24 08:31:06 +0000

    chore: add vpc dhcp options set test

--- internal/provider/resource_vpc_test.go
+++ internal/provider/resource_vpc_test.go
@@ -61,6 +61,72 @@ resource "numspot_vpc" "test" {
 }`, ipRange)
 }
 
+func TestAccNetResource_DhcpOptionsSet(t *testing.T) {
+	t.Parallel()
+
+	domainName := "foo.bar"
+	ipRange := "10.101.0.0/16"
+	ipRangeUpdated := "10.102.0.0/16"
+
+	previousId := ""
+
+	pr := TestAccProtoV6ProviderFactories
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: pr,
+		Steps: []resource.TestStep{
+			{
+				Config: testNetConfig_DhcpOptionsSet(domainName, ipRange),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("numspot_vpc.test", "ip_range", ipRange),
+					resource.TestCheckResourceAttrWith("numspot_vpc.test", "id", func(v string) error {
+						require.NotEmpty(t, v)
+						return nil
+					}),
+					resource.TestCheckResourceAttrWith("numspot_vpc.test", "dhcp_options_set_id", func(v string) error {
+						require.NotEmpty(t, v)
+						previousId = v
+						return nil
+					}),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:            "numspot_vpc.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+			},
+			// Update testing
+			{
+				Config: testNetConfig_DhcpOptionsSet(domainName, ipRangeUpdated),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("numspot_vpc.test", "ip_range", ipRangeUpdated),
+					resource.TestCheckResourceAttrWith("numspot_vpc.test", "id", func(v string) error {
+						require.NotEqual(t, previousId, v)
+						return nil
+					}),
+					resource.TestCheckResourceAttrWith("numspot_vpc.test", "dhcp_options_set_id", func(v string) error {
+						require.NotEmpty(t, v)
+						return nil
+					}),
+				),
+			},
+		},
+	})
+}
+
+func testNetConfig_DhcpOptionsSet(domainName, ipRange string) string {
+	return fmt.Sprintf(`
+resource "numspot_dhcp_options" "test" {
+  domain_name = %[1]q
+}
+
+resource "numspot_vpc" "test" {
+  ip_range            = %[2]q
+  dhcp_options_set_id = numspot_dhcp_options.test.id
+}`, domainName, ipRange)
+}
+
 func TestAccNetResource_Tags(t *testing.T) {
 	t.Parallel()
 	tagValue := "Terraform Provider VPC"
