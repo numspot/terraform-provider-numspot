346f01d2cd80164cba3b8afd7c7cf2ef32d1c706
Safouane Fakhfakh <safouane.fakhfakh@numspot.com>
2024-09-05 10:41:28 +0200

    Link & unlink VM to NIC

--- internal/services/nic/resource_nic_test.go
+++ internal/services/nic/resource_nic_test.go
@@ -3,7 +3,6 @@
 package nic_test
 
 import (
-	"fmt"
 	"slices"
 	"testing"
 
@@ -11,30 +10,81 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/provider"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
-// This struct will store the input data that will be used in your tests (all fields as string)
-type StepDataNic struct {
-	tagKey,
-	tagValue,
-	description string
+type Tag struct {
+	key   string `hcl:"key"`
+	value string `hcl:"value"`
 }
 
+// This struct will store the input data that will be used in your tests (all fields as string)
+type vpc struct {
+	*utils.BlockDefinition
+	ipRange string `hcl:"ip_range,noref"`
+}
+
+// This struct will store the input data that will be used in your tests (all fields as string)
+type subnet struct {
+	*utils.BlockDefinition
+	vpcID   string `hcl:"vpc_id"`
+	ipRange string `hcl:"ip_range,noref"`
+}
+
+type vm struct {
+	*utils.BlockDefinition
+	imageID  string `hcl:"image_id"`
+	vmType   string `hcl:"type"`
+	subnetID string `hcl:"subnet_id"`
+}
+
+type linkNIC struct {
+	vmID         string `hcl:"vm_id"`
+	deviceNumber int    `hcl:"device_number"`
+}
+
+type SGRule struct {
+	fromPortRange int      `hcl:"from_port_range"`
+	toPortRange   int      `hcl:"to_port_range"`
+	ipRanges      []string `hcl:"ip_ranges,noref"`
+	ipProtocol    string   `hcl:"ip_protocol"`
+}
+type securityGroup struct {
+	*utils.BlockDefinition
+	vpcID        string   `hcl:"vpc_id"`
+	name         string   `hcl:"name"`
+	description  string   `hcl:"description"`
+	inboundRules []SGRule `hcl:"inbound_rules"`
+}
+
+type nic struct {
+	*utils.BlockDefinition
+	subnetID    string  `hcl:"subnet_id"`
+	description string  `hcl:"description"`
+	linkNIC     linkNIC `hcl:"link_nic"`
+	// vmName       string `hcl:"vm_name"`
+	deviceNumber     int      `hcl:"device_number"`
+	securityGroupIDs []string `hcl:"security_group_ids"`
+	tags             []Tag    `hcl:"tags"`
+}
+
+var nicResourceID, linkNicID string
+
 // Generate checks to validate that resource 'numspot_nic.test' has input data values
-func getFieldMatchChecksNic(data StepDataNic) []resource.TestCheckFunc {
+func assertionNIC(data nic) []resource.TestCheckFunc {
 	return []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr("numspot_nic.test", "description", data.description),
 		resource.TestCheckResourceAttr("numspot_nic.test", "tags.#", "1"),
 		resource.TestCheckTypeSetElemNestedAttrs("numspot_nic.test", "tags.*", map[string]string{
-			"key":   data.tagKey,
-			"value": data.tagValue,
+			"key":   data.tags[0].key,
+			"value": data.tags[0].value,
 		}),
 	}
 }
 
 // Generate checks to validate that resource 'numspot_nic.test' is properly linked to given subresources
 // If resource has no dependencies, return empty array
-func getDependencyChecksNic(dependenciesSuffix string) []resource.TestCheckFunc {
+func dependencyAssertionNIC(dependenciesSuffix string) []resource.TestCheckFunc {
 	return []resource.TestCheckFunc{
 		resource.TestCheckResourceAttrPair("numspot_nic.test", "subnet_id", "numspot_subnet.test"+dependenciesSuffix, "id"),
 		resource.TestCheckTypeSetElemAttrPair("numspot_nic.test", "security_group_ids.*", "numspot_security_group.test"+dependenciesSuffix, "id"),
@@ -44,79 +94,20 @@ func getDependencyChecksNic(dependenciesSuffix string) []resource.TestCheckFunc 
 func TestAccNicResource(t *testing.T) {
 	pr := provider.TestAccProtoV6ProviderFactories
 
-	var resourceId string
-
-	////////////// Define input data that will be used in the test sequence //////////////
-	// resource fields that can be updated in-place
-	tagKey := "name"
-	tagValue := "Terraform-Test-Volume"
-	tagValueUpdated := "Terraform-Test-Volume-Update"
-
-	description := "The nic"
-	descriptionUpdated := "The better nic"
-	// resource fields that cannot be updated in-place (requires replace)
-	// None
+	createPlan, createChecks := createNIC(t)
+	updatePlan, updateChecks := updateNIC(t)
+	updateLinkUnlinkVMPlan, updateLinkUnlinkVMChecks := updateNICLinkUnlinkVM(t)
+	replacePlan, replaceChecks := replaceNIC(t)
 
 	/////////////////////////////////////////////////////////////////////////////////////
-
-	////////////// Define plan values and generate associated attribute checks  //////////////
-	// The base plan (used in first create and to reset resource state before some tests)
-	basePlanValues := StepDataNic{
-		tagKey:      tagKey,
-		tagValue:    tagValue,
-		description: description,
-	}
-	createChecks := append(
-		getFieldMatchChecksNic(basePlanValues),
-
-		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
-			require.NotEmpty(t, v)
-			resourceId = v
-			return nil
-		}),
-	)
-
-	// The plan that should trigger Update function (based on basePlanValues). Update the value for as much updatable fields as possible here.
-	updatePlanValues := StepDataNic{
-		tagKey:      tagKey,
-		tagValue:    tagValueUpdated,
-		description: descriptionUpdated,
-	}
-	updateChecks := append(
-		getFieldMatchChecksNic(updatePlanValues),
-
-		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
-			require.NotEmpty(t, v)
-			require.Equal(t, v, resourceId)
-			return nil
-		}),
-	)
-
-	// The plan that should trigger Replace behavior (based on basePlanValues or updatePlanValues). Update the value for as much non-updatable fields as possible here.
-	replacePlanValues := StepDataNic{ // Replace is triggered by a dependency change (subnet)
-		tagKey:      tagKey,
-		tagValue:    tagValue,
-		description: description,
-	}
-	replaceChecks := append(
-		getFieldMatchChecksNic(replacePlanValues),
-
-		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
-			require.NotEmpty(t, v)
-			require.NotEqual(t, v, resourceId)
-			return nil
-		}),
-	)
-	/////////////////////////////////////////////////////////////////////////////////////
-
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: pr,
 		Steps: []resource.TestStep{
 			{ // Create testing
-				Config: testNicConfig(provider.BASE_SUFFIX, basePlanValues),
+				Config: createPlan,
 				Check: resource.ComposeAggregateTestCheckFunc(slices.Concat(
 					createChecks,
-					getDependencyChecksNic(provider.BASE_SUFFIX),
+					dependencyAssertionNIC(provider.BASE_SUFFIX),
 				)...),
 			},
 			// ImportState testing
@@ -128,94 +119,409 @@ func TestAccNicResource(t *testing.T) {
 			},
 			// Update testing Without Replace (if needed)
 			{
-				Config: testNicConfig(provider.BASE_SUFFIX, updatePlanValues),
+				Config: updatePlan,
 				Check: resource.ComposeAggregateTestCheckFunc(slices.Concat(
 					updateChecks,
-					getDependencyChecksNic(provider.BASE_SUFFIX),
+					dependencyAssertionNIC(provider.BASE_SUFFIX),
 				)...),
 			},
-
-			// <== If resource has required dependencies ==>
-			{ // Reset the resource to initial state (resource tied to a subresource) in prevision of next test
-				Config: testNicConfig(provider.BASE_SUFFIX, basePlanValues),
-			},
-			// Update testing With Replace of dependency resource and with Replace of the resource (if needed)
-			// This test is useful to check wether or not the deletion of the dependencies and then the deletion of the main resource works properly
+			//// Update testing with unlink old VM and link new VM
 			{
-				Config: testNicConfig(provider.NEW_SUFFIX, replacePlanValues),
+				Config: updateLinkUnlinkVMPlan,
+				Check: resource.ComposeAggregateTestCheckFunc(slices.Concat(
+					updateLinkUnlinkVMChecks,
+					dependencyAssertionNIC(provider.BASE_SUFFIX),
+				)...),
+			},
+			{
+				Config: replacePlan,
 				Check: resource.ComposeAggregateTestCheckFunc(slices.Concat(
 					replaceChecks,
-					getDependencyChecksNic(provider.NEW_SUFFIX),
+					dependencyAssertionNIC(provider.NEW_SUFFIX),
 				)...),
 			},
-
-			// <== If resource has optional dependencies ==>
-			// --> DELETED TEST <-- : due to Numspot APIs architecture, this use case will not work in most cases. Nothing can be done on provider side to fix this
-			// Update testing With Deletion of dependency resource and with Replace of the resource (if needed)
-			// This test is useful to check wether or not the deletion of the dependencies and then the replace of the main resource works properly (empty dependency)
 		},
 	})
 }
 
-func testNicConfig(subresourceSuffix string, data StepDataNic) string {
-	return fmt.Sprintf(`
-resource "numspot_vpc" "test" {
-  ip_range = "10.101.0.0/16"
+func createNIC(t *testing.T) (string, []resource.TestCheckFunc) {
+	vpcObj := vpc{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vpc",
+			Labels: []string{"test"},
+		},
+		ipRange: "10.101.0.0/16",
+	}
+	subnetObj := subnet{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_subnet",
+			Labels: []string{"test"},
+		},
+		vpcID:   "numspot_vpc.test.id",
+		ipRange: "10.101.1.0/24",
+	}
+	sgObj := securityGroup{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_security_group",
+			Labels: []string{"test"},
+		},
+		vpcID:       "numspot_vpc.test.id",
+		name:        "security_group",
+		description: "numspot_security_group description",
+		inboundRules: []SGRule{
+			{
+				fromPortRange: 80,
+				toPortRange:   80,
+				ipRanges:      []string{"0.0.0.0/0"},
+				ipProtocol:    "tcp",
+			},
+		},
+	}
+	vmObj := vm{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vm",
+			Labels: []string{"test"},
+		},
+		subnetID: "numspot_subnet.test.id",
+		vmType:   "ns-cus6-2c4r",
+		imageID:  "ami-0b7df82c",
+	}
+	nicObj := nic{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_nic",
+			Labels: []string{"test"},
+		},
+		linkNIC: linkNIC{
+			deviceNumber: 1,
+			vmID:         "numspot_vm.test.id",
+		},
+		subnetID:         "numspot_subnet.test.id",
+		description:      "The nic",
+		deviceNumber:     1,
+		securityGroupIDs: []string{"numspot_security_group.test.id"},
+		tags: []Tag{
+			{
+				key:   "name",
+				value: "Terraform-Test-Volume",
+			},
+		},
+	}
+
+	checks := append(
+		assertionNIC(nicObj),
+
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
+			require.NotEmpty(t, v)
+			nicResourceID = v
+			return nil
+		}),
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "link_nic.id", func(v string) error {
+			linkNicID = v
+			require.NotEmpty(t, v)
+			return nil
+		}),
+	)
+
+	vpcHCL, err := utils.Marshal(&vpcObj)
+	require.NoError(t, err)
+	subnetHCL, err := utils.Marshal(&subnetObj)
+	require.NoError(t, err)
+	sgHCL, err := utils.Marshal(&sgObj)
+	require.NoError(t, err)
+	vmHCL, err := utils.Marshal(&vmObj)
+	require.NoError(t, err)
+	nicHCL, err := utils.Marshal(&nicObj)
+	require.NoError(t, err)
+
+	plan := string(vpcHCL) + string(subnetHCL) + string(sgHCL) + string(vmHCL) + string(nicHCL)
+	return plan, checks
 }
 
-resource "numspot_subnet" "test%[1]s" {
-  vpc_id   = numspot_vpc.test.id
-  ip_range = "10.101.1.0/24"
+func updateNIC(t *testing.T) (string, []resource.TestCheckFunc) {
+	vpcObj := vpc{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vpc",
+			Labels: []string{"test"},
+		},
+		ipRange: "10.101.0.0/16",
+	}
+	subnetObj := subnet{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_subnet",
+			Labels: []string{"test"},
+		},
+		vpcID:   "numspot_vpc.test.id",
+		ipRange: "10.101.1.0/24",
+	}
+	sgObj := securityGroup{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_security_group",
+			Labels: []string{"test"},
+		},
+		vpcID:       "numspot_vpc.test.id",
+		name:        "security_group",
+		description: "numspot_security_group description",
+		inboundRules: []SGRule{
+			{
+				fromPortRange: 80,
+				toPortRange:   80,
+				ipRanges:      []string{"0.0.0.0/0"},
+				ipProtocol:    "tcp",
+			},
+		},
+	}
+	vmObj := vm{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vm",
+			Labels: []string{"test"},
+		},
+		subnetID: "numspot_subnet.test.id",
+		vmType:   "ns-cus6-2c4r",
+		imageID:  "ami-0b7df82c",
+	}
+	nicObj := nic{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_nic",
+			Labels: []string{"test"},
+		},
+		linkNIC: linkNIC{
+			deviceNumber: 1,
+			vmID:         "numspot_vm.test.id",
+		},
+		subnetID:         "numspot_subnet.test.id",
+		description:      "The better nic",
+		deviceNumber:     1,
+		securityGroupIDs: []string{"numspot_security_group.test.id"},
+		tags: []Tag{
+			{
+				key:   "name",
+				value: "Terraform-Test-Volume-Update",
+			},
+		},
+	}
+
+	checks := append(
+		assertionNIC(nicObj),
+
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
+			require.NotEmpty(t, v)
+			require.Equal(t, v, nicResourceID)
+			return nil
+		}),
+	)
+
+	vpcHCL, err := utils.Marshal(&vpcObj)
+	require.NoError(t, err)
+	subnetHCL, err := utils.Marshal(&subnetObj)
+	require.NoError(t, err)
+	sgHCL, err := utils.Marshal(&sgObj)
+	require.NoError(t, err)
+	vmHCL, err := utils.Marshal(&vmObj)
+	require.NoError(t, err)
+	nicHCL, err := utils.Marshal(&nicObj)
+	require.NoError(t, err)
+
+	plan := string(vpcHCL) + string(subnetHCL) + string(sgHCL) + string(vmHCL) + string(nicHCL)
+	return plan, checks
 }
 
-resource "numspot_security_group" "test%[1]s" {
-  vpc_id      = numspot_vpc.test.id
-  name        = "security_group"
-  description = "numspot_security_group description"
+func updateNICLinkUnlinkVM(t *testing.T) (string, []resource.TestCheckFunc) {
+	vpcObj := vpc{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vpc",
+			Labels: []string{"test"},
+		},
+		ipRange: "10.101.0.0/16",
+	}
+	subnetObj := subnet{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_subnet",
+			Labels: []string{"test"},
+		},
+		vpcID:   "numspot_vpc.test.id",
+		ipRange: "10.101.1.0/24",
+	}
+	sgObj := securityGroup{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_security_group",
+			Labels: []string{"test"},
+		},
+		vpcID:       "numspot_vpc.test.id",
+		name:        "security_group",
+		description: "numspot_security_group description",
+		inboundRules: []SGRule{
+			{
+				fromPortRange: 80,
+				toPortRange:   80,
+				ipRanges:      []string{"0.0.0.0/0"},
+				ipProtocol:    "tcp",
+			},
+		},
+	}
+	vmObj := vm{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vm",
+			Labels: []string{"test-updated"},
+		},
+		subnetID: "numspot_subnet.test.id",
+		vmType:   "ns-cus6-2c4r",
+		imageID:  "ami-0b7df82c",
+	}
+	nicObj := nic{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_nic",
+			Labels: []string{"test"},
+		},
+		linkNIC: linkNIC{
+			deviceNumber: 1,
+			vmID:         "numspot_vm.test-updated.id",
+		},
+		subnetID:         "numspot_subnet.test.id",
+		description:      "The better nic",
+		deviceNumber:     1,
+		securityGroupIDs: []string{"numspot_security_group.test.id"},
+		tags: []Tag{
+			{
+				key:   "name",
+				value: "Terraform-Test-Volume-Update",
+			},
+		},
+	}
 
-  inbound_rules = [
-    {
-      from_port_range = 80
-      to_port_range   = 80
-      ip_ranges       = ["0.0.0.0/0"]
-      ip_protocol     = "tcp"
-    }
-  ]
+	checks := append(
+		assertionNIC(nicObj),
+
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
+			require.NotEmpty(t, v)
+			require.Equal(t, v, nicResourceID)
+			return nil
+		}),
+	)
+
+	vpcHCL, err := utils.Marshal(&vpcObj)
+	require.NoError(t, err)
+	subnetHCL, err := utils.Marshal(&subnetObj)
+	require.NoError(t, err)
+	sgHCL, err := utils.Marshal(&sgObj)
+	require.NoError(t, err)
+	vmHCL, err := utils.Marshal(&vmObj)
+	require.NoError(t, err)
+	nicHCL, err := utils.Marshal(&nicObj)
+	require.NoError(t, err)
+
+	plan := string(vpcHCL) + string(subnetHCL) + string(sgHCL) + string(vmHCL) + string(nicHCL)
+	return plan, checks
 }
 
-resource "numspot_nic" "test" {
-  subnet_id          = numspot_subnet.test%[1]s.id
-  description        = %[2]q
-  security_group_ids = [numspot_security_group.test%[1]s.id]
-  tags = [
-    {
-      key   = %[3]q
-      value = %[4]q
-    }
-  ]
-}`, subresourceSuffix, data.description, data.tagKey, data.tagValue)
-}
+func replaceNIC(t *testing.T) (string, []resource.TestCheckFunc) {
+	vpcObj := vpc{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vpc",
+			Labels: []string{"testnew"},
+		},
+		ipRange: "10.101.0.0/16",
+	}
+	subnetObj := subnet{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_subnet",
+			Labels: []string{"testnew"},
+		},
+		vpcID:   "numspot_vpc.testnew.id",
+		ipRange: "10.101.1.0/24",
+	}
+	sgObj := securityGroup{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_security_group",
+			Labels: []string{"testnew"},
+		},
+		vpcID:       "numspot_vpc.testnew.id",
+		name:        "security_group",
+		description: "numspot_security_group description",
+		inboundRules: []SGRule{
+			{
+				fromPortRange: 80,
+				toPortRange:   80,
+				ipRanges:      []string{"0.0.0.0/0"},
+				ipProtocol:    "tcp",
+			},
+		},
+	}
+	vmObj := vm{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_vm",
+			Labels: []string{"testnew"},
+		},
+		subnetID: "numspot_subnet.testnew.id",
+		vmType:   "ns-cus6-2c4r",
+		imageID:  "ami-0b7df82c",
+	}
+	nicObj := nic{
+		BlockDefinition: &utils.BlockDefinition{
+			Type:   "resource",
+			Name:   "numspot_nic",
+			Labels: []string{"test"},
+		},
+		linkNIC: linkNIC{
+			deviceNumber: 1,
+			vmID:         "numspot_vm.testnew.id",
+		},
+		subnetID:         "numspot_subnet.testnew.id",
+		description:      "The better nic",
+		deviceNumber:     1,
+		securityGroupIDs: []string{"numspot_security_group.testnew.id"},
+		tags: []Tag{
+			{
+				key:   "name",
+				value: "Terraform-Test-Volume-Update",
+			},
+		},
+	}
 
-// <== If resource has optional dependencies ==>
-func testNicConfig_DeletedDependencies(data StepDataNic) string {
-	return fmt.Sprintf(`
-resource "numspot_vpc" "test" {
-  ip_range = "10.101.0.0/16"
-}
+	checks := append(
+		assertionNIC(nicObj),
 
-resource "numspot_subnet" "test" {
-  vpc_id   = numspot_vpc.test.id
-  ip_range = "10.101.1.0/24"
-}
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "id", func(v string) error {
+			require.NotEmpty(t, v)
+			require.NotEqual(t, v, nicResourceID)
+			return nil
+		}),
+		resource.TestCheckResourceAttrWith("numspot_nic.test", "link_nic.id", func(v string) error {
+			require.NotEmpty(t, v)
+			require.NotEqual(t, v, linkNicID)
+			return nil
+		}),
+	)
 
-resource "numspot_nic" "test" {
-  subnet_id   = numspot_subnet.test.id
-  description = %[1]q
-  tags = [
-    {
-      key   = %[2]q
-      value = %[3]q
-    }
-  ]
-}`, data.description, data.tagKey, data.tagValue)
+	vpcHCL, err := utils.Marshal(&vpcObj)
+	require.NoError(t, err)
+	subnetHCL, err := utils.Marshal(&subnetObj)
+	require.NoError(t, err)
+	sgHCL, err := utils.Marshal(&sgObj)
+	require.NoError(t, err)
+	vmHCL, err := utils.Marshal(&vmObj)
+	require.NoError(t, err)
+	nicHCL, err := utils.Marshal(&nicObj)
+	require.NoError(t, err)
+
+	plan := string(vpcHCL) + string(subnetHCL) + string(sgHCL) + string(vmHCL) + string(nicHCL)
+	return plan, checks
 }
