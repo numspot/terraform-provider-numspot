1cb6fa4e1befa794506aacf04e9376fe9d3dc66a
Dimitri David <dimitri.david@numspot.com>
2024-10-03 09:24:27 +0200

    WIP on feature/new-resource-template

--- internal/services/dhcpoptions/resource_dhcp_options.go
+++ internal/services/dhcpoptions/resource_dhcp_options.go
@@ -3,21 +3,24 @@ package dhcpoptions
 import (
 	"context"
 	"fmt"
-	"net/http"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/resourcevalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"gitlab.numspot.cloud/cloud/numspot-sdk-go/pkg/numspot"
-
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/core"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/services"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/services/tags"
-	utils2 "gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
+	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
 var (
-	_ resource.Resource                = &DhcpOptionsResource{}
-	_ resource.ResourceWithConfigure   = &DhcpOptionsResource{}
-	_ resource.ResourceWithImportState = &DhcpOptionsResource{}
+	_ resource.Resource                     = &DhcpOptionsResource{}
+	_ resource.ResourceWithConfigure        = &DhcpOptionsResource{}
+	_ resource.ResourceWithImportState      = &DhcpOptionsResource{}
+	_ resource.ResourceWithConfigValidators = &DhcpOptionsResource{}
 )
 
 type DhcpOptionsResource struct {
@@ -28,7 +31,7 @@ func NewDhcpOptionsResource() resource.Resource {
 	return &DhcpOptionsResource{}
 }
 
-func (r *DhcpOptionsResource) Configure(ctx context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
+func (r *DhcpOptionsResource) Configure(_ context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
 	if request.ProviderData == nil {
 		return
 	}
@@ -50,118 +53,185 @@ func (r *DhcpOptionsResource) ImportState(ctx context.Context, request resource.
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), request, response)
 }
 
-func (r *DhcpOptionsResource) Metadata(ctx context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
+func (r *DhcpOptionsResource) Metadata(_ context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
 	response.TypeName = request.ProviderTypeName + "_dhcp_options"
 }
 
-func (r *DhcpOptionsResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
+func (r *DhcpOptionsResource) Schema(ctx context.Context, _ resource.SchemaRequest, response *resource.SchemaResponse) {
 	response.Schema = DhcpOptionsResourceSchema(ctx)
 }
 
+func (r *DhcpOptionsResource) ConfigValidators(_ context.Context) []resource.ConfigValidator {
+	return []resource.ConfigValidator{
+		resourcevalidator.AtLeastOneOf(
+			path.Root("domain_name").Expression(),
+			path.Root("domain_name_servers").Expression(),
+			path.Root("log_servers").Expression(),
+			path.Root("ntp_servers").Expression(),
+		),
+	}
+}
+
 func (r *DhcpOptionsResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
-	var data DhcpOptionsModel
-	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
-	if response.Diagnostics.HasError() {
-		return
-	}
-
-	// Retries create until request response is OK
-	res, err := utils2.RetryCreateUntilResourceAvailableWithBody(
-		ctx,
-		r.provider.GetSpaceID(),
-		DhcpOptionsFromTfToCreateRequest(ctx, data),
-		r.provider.GetNumspotClient().CreateDhcpOptionsWithResponse)
-	if err != nil {
-		response.Diagnostics.AddError("Failed to create DHCP Options", err.Error())
-		return
-	}
-
-	createdId := *res.JSON201.Id
-	if len(data.Tags.Elements()) > 0 {
-		tags.CreateTagsFromTf(ctx, r.provider.GetNumspotClient(), r.provider.GetSpaceID(), &response.Diagnostics, createdId, data.Tags)
-		if response.Diagnostics.HasError() {
-			return
-		}
-	}
-
-	tf, diagnostics := DhcpOptionsFromHttpToTf(ctx, res.JSON201)
-	tf.Tags = tags.ReadTags(ctx, r.provider.GetNumspotClient(), r.provider.GetSpaceID(), response.Diagnostics, createdId)
-	if diagnostics.HasError() {
-		response.Diagnostics.Append(diagnostics...)
-		return
-	}
-
-	response.Diagnostics.Append(response.State.Set(ctx, &tf)...)
-}
-
-func (r *DhcpOptionsResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
-	var data DhcpOptionsModel
-	response.Diagnostics.Append(request.State.Get(ctx, &data)...)
-
-	res := utils2.ExecuteRequest(func() (*numspot.ReadDhcpOptionsByIdResponse, error) {
-		return r.provider.GetNumspotClient().ReadDhcpOptionsByIdWithResponse(ctx, r.provider.GetSpaceID(), data.Id.ValueString())
-	}, http.StatusOK, &response.Diagnostics)
-	if res == nil {
-		return
-	}
-
-	tf, diagnostics := DhcpOptionsFromHttpToTf(ctx, res.JSON200)
-	if diagnostics.HasError() {
-		response.Diagnostics.Append(diagnostics...)
-		return
-	}
-	response.Diagnostics.Append(response.State.Set(ctx, &tf)...)
-}
-
-func (r *DhcpOptionsResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
-	var plan, state DhcpOptionsModel
-
-	response.Diagnostics.Append(request.State.Get(ctx, &state)...)
+	var plan DhcpOptionsModel
 	response.Diagnostics.Append(request.Plan.Get(ctx, &plan)...)
 	if response.Diagnostics.HasError() {
 		return
 	}
 
-	if !state.Tags.Equal(plan.Tags) {
-		tags.UpdateTags(
-			ctx,
-			state.Tags,
-			plan.Tags,
-			&response.Diagnostics,
-			r.provider.GetNumspotClient(),
-			r.provider.GetSpaceID(),
-			state.Id.ValueString(),
-		)
-		if response.Diagnostics.HasError() {
-			return
-		}
-	}
+	tagsValue := tags.TfTagsToApiTags(ctx, plan.Tags)
 
-	res := utils2.ExecuteRequest(func() (*numspot.ReadDhcpOptionsByIdResponse, error) {
-		return r.provider.GetNumspotClient().ReadDhcpOptionsByIdWithResponse(ctx, r.provider.GetSpaceID(), state.Id.ValueString())
-	}, http.StatusOK, &response.Diagnostics)
-	if res == nil {
+	numSpotDHCPOptions, err := core.CreateDHCPOptions(ctx, r.provider, deserializeDHCPOptions(ctx, plan), tagsValue)
+	if err != nil {
+		response.Diagnostics.AddError("unable to create DHCP options", err.Error())
 		return
 	}
 
-	tf, diags := DhcpOptionsFromHttpToTf(ctx, res.JSON200)
+	state, diags := serializeNumSpotDHCPOptions(ctx, numSpotDHCPOptions)
 	if diags.HasError() {
+		response.Diagnostics.Append(diags...)
 		return
 	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &state)...)
+}
+
+func (r *DhcpOptionsResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
+	var state DhcpOptionsModel
+	response.Diagnostics.Append(request.State.Get(ctx, &state)...)
 	if response.Diagnostics.HasError() {
 		return
 	}
 
-	response.Diagnostics.Append(response.State.Set(ctx, &tf)...)
+	dhcpOptionsID := state.Id.ValueString()
+
+	dhcpOptions, err := core.ReadDHCPOptions(ctx, r.provider, dhcpOptionsID)
+	if err != nil {
+		response.Diagnostics.AddError("", "")
+		return
+	}
+
+	newState, diagnostics := serializeNumSpotDHCPOptions(ctx, dhcpOptions)
+	if diagnostics.HasError() {
+		response.Diagnostics.Append(diagnostics...)
+		return
+	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &newState)...)
+}
+
+func (r *DhcpOptionsResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
+	var (
+		err                error
+		numSpotDHCPOptions *numspot.DhcpOptionsSet
+		state, plan        DhcpOptionsModel
+		diags              diag.Diagnostics
+	)
+
+	response.Diagnostics.Append(request.State.Get(ctx, &state)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	response.Diagnostics.Append(request.Plan.Get(ctx, &plan)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	dhcpOptionsID := state.Id.ValueString()
+	stateTags := tags.TfTagsToApiTags(ctx, state.Tags)
+	planTags := tags.TfTagsToApiTags(ctx, plan.Tags)
+
+	if !plan.Tags.Equal(state.Tags) {
+		numSpotDHCPOptions, err = core.UpdateDHCPOptionsTags(ctx, r.provider, dhcpOptionsID, stateTags, planTags)
+		if err != nil {
+			response.Diagnostics.AddError("unable to update DHCP options tags", err.Error())
+			return
+		}
+	}
+
+	newState, diags := serializeNumSpotDHCPOptions(ctx, numSpotDHCPOptions)
+	if diags.HasError() {
+		return
+	}
+
+	response.Diagnostics.Append(response.State.Set(ctx, &newState)...)
 }
 
 func (r *DhcpOptionsResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
-	var data DhcpOptionsModel
-	response.Diagnostics.Append(request.State.Get(ctx, &data)...)
-
-	err := utils2.RetryDeleteUntilResourceAvailable(ctx, r.provider.GetSpaceID(), data.Id.ValueString(), r.provider.GetNumspotClient().DeleteDhcpOptionsWithResponse)
-	if err != nil {
-		response.Diagnostics.AddError("Failed to delete DHCP Options", err.Error())
+	var state DhcpOptionsModel
+	response.Diagnostics.Append(request.State.Get(ctx, &state)...)
+	if response.Diagnostics.HasError() {
 		return
 	}
+
+	dhcpOptionsID := state.Id.ValueString()
+
+	if err := core.DeleteDHCPOptions(ctx, r.provider, dhcpOptionsID); err != nil {
+		response.Diagnostics.AddError("failed to delete DHCP Options", err.Error())
+		return
+	}
+}
+
+func deserializeDHCPOptions(ctx context.Context, tf DhcpOptionsModel) numspot.CreateDhcpOptionsJSONRequestBody {
+	var domainNameServers, logServers, ntpServers []string
+
+	domainNameServers = make([]string, 0, len(tf.DomainNameServers.Elements()))
+	tf.DomainNameServers.ElementsAs(ctx, &domainNameServers, false)
+
+	logServers = make([]string, 0, len(tf.LogServers.Elements()))
+	tf.LogServers.ElementsAs(ctx, &logServers, false)
+
+	ntpServers = make([]string, 0, len(tf.NtpServers.Elements()))
+	tf.NtpServers.ElementsAs(ctx, &ntpServers, false)
+
+	return numspot.CreateDhcpOptionsJSONRequestBody{
+		DomainName:        tf.DomainName.ValueStringPointer(),
+		DomainNameServers: &domainNameServers,
+		LogServers:        &logServers,
+		NtpServers:        &ntpServers,
+	}
+}
+
+func serializeNumSpotDHCPOptions(ctx context.Context, http *numspot.DhcpOptionsSet) (DhcpOptionsModel, diag.Diagnostics) {
+	var diagnostics diag.Diagnostics
+	var domainNameServersTf, logServersTf, ntpServersTf, tagsTf types.List
+
+	if http.DomainNameServers != nil {
+		domainNameServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.DomainNameServers)
+		if diagnostics.HasError() {
+			return DhcpOptionsModel{}, diagnostics
+		}
+	}
+
+	if http.LogServers != nil {
+		logServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.LogServers)
+		if diagnostics.HasError() {
+			return DhcpOptionsModel{}, diagnostics
+		}
+	}
+
+	if http.NtpServers != nil {
+		ntpServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.NtpServers)
+		if diagnostics.HasError() {
+			return DhcpOptionsModel{}, diagnostics
+		}
+	}
+
+	if http.Tags != nil {
+		tagsTf, diagnostics = utils.GenericListToTfListValue(ctx, tags.TagsValue{}, tags.ResourceTagFromAPI, *http.Tags)
+		if diagnostics.HasError() {
+			return DhcpOptionsModel{}, diagnostics
+		}
+	}
+
+	return DhcpOptionsModel{
+		Default:           types.BoolPointerValue(http.Default),
+		DomainName:        types.StringPointerValue(http.DomainName),
+		Id:                types.StringPointerValue(http.Id),
+		DomainNameServers: domainNameServersTf,
+		LogServers:        logServersTf,
+		NtpServers:        ntpServersTf,
+		Tags:              tagsTf,
+	}, nil
 }
--- internal/services/dhcpoptions/utils.go
+++ internal/services/dhcpoptions/utils.go
@@ -6,73 +6,52 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"gitlab.numspot.cloud/cloud/numspot-sdk-go/pkg/numspot"
-
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/services/tags"
 	"gitlab.numspot.cloud/cloud/terraform-provider-numspot/internal/utils"
 )
 
-func DhcpOptionsFromHttpToTf(ctx context.Context, http *numspot.DhcpOptionsSet) (*DhcpOptionsModel, diag.Diagnostics) {
-	var diagnostics diag.Diagnostics
-	var domainNameServersTf, logServersTf, ntpServersTf, tagsTf types.List
-
-	if http.DomainNameServers != nil {
-		domainNameServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.DomainNameServers)
-		if diagnostics.HasError() {
-			return nil, diagnostics
-		}
-	}
-
-	if http.LogServers != nil {
-		logServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.LogServers)
-		if diagnostics.HasError() {
-			return nil, diagnostics
-		}
-	}
-
-	if http.NtpServers != nil {
-		ntpServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.LogServers)
-		if diagnostics.HasError() {
-			return nil, diagnostics
-		}
-	}
-
-	if http.Tags != nil {
-		tagsTf, diagnostics = utils.GenericListToTfListValue(ctx, tags.TagsValue{}, tags.ResourceTagFromAPI, *http.Tags)
-		if diagnostics.HasError() {
-			return nil, diagnostics
-		}
-	}
-
-	return &DhcpOptionsModel{
-		Default:           types.BoolPointerValue(http.Default),
-		DomainName:        types.StringPointerValue(http.DomainName),
-		Id:                types.StringPointerValue(http.Id),
-		DomainNameServers: domainNameServersTf,
-		LogServers:        logServersTf,
-		NtpServers:        ntpServersTf,
-		Tags:              tagsTf,
-	}, nil
-}
-
-func DhcpOptionsFromTfToCreateRequest(ctx context.Context, tf DhcpOptionsModel) numspot.CreateDhcpOptionsJSONRequestBody {
-	var domainNameServers, logServers, ntpServers []string
-
-	domainNameServers = make([]string, 0, len(tf.DomainNameServers.Elements()))
-	tf.DomainNameServers.ElementsAs(ctx, &domainNameServers, false)
-
-	logServers = make([]string, 0, len(tf.LogServers.Elements()))
-	tf.LogServers.ElementsAs(ctx, &logServers, false)
-
-	ntpServers = make([]string, 0, len(tf.NtpServers.Elements()))
-	tf.NtpServers.ElementsAs(ctx, &ntpServers, false)
-
-	return numspot.CreateDhcpOptionsJSONRequestBody{
-		DomainName:        tf.DomainName.ValueStringPointer(),
-		DomainNameServers: &domainNameServers,
-		LogServers:        &logServers,
-		NtpServers:        &ntpServers,
-	}
-}
+//func DhcpOptionsFromHttpToTf(ctx context.Context, http *numspot.DhcpOptionsSet) (*DhcpOptionsModel, diag.Diagnostics) {
+//	var diagnostics diag.Diagnostics
+//	var domainNameServersTf, logServersTf, ntpServersTf, tagsTf types.List
+//
+//	if http.DomainNameServers != nil {
+//		domainNameServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.DomainNameServers)
+//		if diagnostics.HasError() {
+//			return nil, diagnostics
+//		}
+//	}
+//
+//	if http.LogServers != nil {
+//		logServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.LogServers)
+//		if diagnostics.HasError() {
+//			return nil, diagnostics
+//		}
+//	}
+//
+//	if http.NtpServers != nil {
+//		ntpServersTf, diagnostics = utils.StringListToTfListValue(ctx, *http.LogServers)
+//		if diagnostics.HasError() {
+//			return nil, diagnostics
+//		}
+//	}
+//
+//	if http.Tags != nil {
+//		tagsTf, diagnostics = utils.GenericListToTfListValue(ctx, tags.TagsValue{}, tags.ResourceTagFromAPI, *http.Tags)
+//		if diagnostics.HasError() {
+//			return nil, diagnostics
+//		}
+//	}
+//
+//	return &DhcpOptionsModel{
+//		Default:           types.BoolPointerValue(http.Default),
+//		DomainName:        types.StringPointerValue(http.DomainName),
+//		Id:                types.StringPointerValue(http.Id),
+//		DomainNameServers: domainNameServersTf,
+//		LogServers:        logServersTf,
+//		NtpServers:        ntpServersTf,
+//		Tags:              tagsTf,
+//	}, nil
+//}
 
 func DhcpOptionsFromTfToAPIReadParams(ctx context.Context, tf DHCPOptionsDataSourceModel) numspot.ReadDhcpOptionsParams {
 	ids := utils.TfStringListToStringPtrList(ctx, tf.IDs)
